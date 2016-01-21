138eda8e9000b6530bea1c4b3e72cfe76a2beca3
Diogo Silva <diogo@diogok.net>
2016-01-21 11:39:41 -0200

    more stats

--- html/app.php
+++ html/app.php
@@ -21,6 +21,94 @@ $app->get('/',function($req,$res) {
   $params = [ 'index'=>INDEX];
   $props['analysis_count']=$es->count($params)['count'] - $props['taxa_count'] - $props['occurrence_count'];
 
+  $params = [
+    'index'=>INDEX,
+    'size'=>0,
+    'fields'=>[],
+    'body'=> [
+      'size'=>0,
+      'query'=>['query_string'=>['analyze_wildcard'=>true,'query'=>'*']],
+      'aggs'=>[
+       'categories'=>['terms'=>[ 'field'=>'risk-assessment.category','size'=>0]],
+       'occs_count'=>['sum'=>['field'=>'occurrences.count']],
+       'points_count'=>['sum'=>['field'=>'points.count']],
+       'occs_ranges'=>[
+         'range'=>[ 
+           'field'=>'occurrences.count',
+           'ranges'=> [ ["from"=>0,"to"=>1]
+                       ,["from"=>1,"to"=>3]
+                       ,["from"=>3,"to"=>10]
+                       ,["from"=>10,"to"=>100]
+                       ,["from"=>100,"to"=>99999]]
+         ]],
+       'points_ranges'=>[
+         'range'=>[ 
+           'field'=>'points.count',
+           'ranges'=> [ ["from"=>0,"to"=>1]
+                       ,["from"=>1,"to"=>3]
+                       ,["from"=>3,"to"=>10]
+                       ,["from"=>10,"to"=>100]
+                       ,["from"=>100,"to"=>99999]]]],
+       'aoo'=>[
+         'filter'=>['term'=>['_type'=>'aoo']],
+         'aggs'=>[
+           'aoo_ranges'=>[
+             'range'=>[ 
+               'field'=>'area',
+               'ranges'=> [ ["from"=>0,"to"=>1]
+                           ,["from"=>1,"to"=>10]
+                           ,["from"=>10,"to"=>50]
+                           ,["from"=>50,"to"=>100]
+                           ,["from"=>100,"to"=>500]
+                           ,["from"=>500,"to"=>2000]
+                           ,["from"=>2000,"to"=>5000]
+                           ,["from"=>5000,"to"=>99999]]]]]],
+       'eoo'=>[
+         'filter'=>['term'=>['_type'=>'eoo']],
+         'aggs'=>[
+           'eoo_ranges'=>[
+             'range'=>[ 
+               'field'=>'geo.properties.area',
+               'ranges'=> [ ["from"=>0,"to"=>1]
+                           ,["from"=>1,"to"=>100]
+                           ,["from"=>100,"to"=>500]
+                           ,["from"=>500,"to"=>5000]
+                           ,["from"=>5000,"to"=>20000]
+                           ,["from"=>20000,"to"=>50000]
+                           ,["from"=>50000,"to"=>999999]]]]]],
+       ]]]; 
+
+   try {
+     $r = $es->search($params);
+   } catch(Exception $e) {
+     var_dump($e);
+     $r['aggregations']=[];
+   }
+
+  $stats=[];
+  foreach($r['aggregations'] as $k=>$agg) {
+    if(isset($agg['buckets'])) {
+      $stats[$k]=[];
+      foreach($agg['buckets'] as $value) {
+        $stats[$k][]=['label'=>strtoupper($value['key']),'value'=>$value['doc_count']];
+      }
+    } else if(isset($agg['value'])) {
+      $stats[$k]=$agg['value'];
+    } else{
+      foreach($agg as $rek=>$reagg) {
+        if(isset($reagg['buckets'])) {
+          $stats[$rek]=[];
+          foreach($reagg['buckets'] as $value) {
+            $stats[$rek][]=['label'=>strtoupper($value['key']),'value'=>$value['doc_count']];
+          }
+        }
+      }
+    }
+  }
+
+  $props['stats']=$stats;
+  $props['stats_json']=json_encode($stats);
+
   $res->getBody()->write(view('index',$props));
   return $res;
 });
@@ -106,7 +194,7 @@ $app->get('/taxon/{taxon}',function($req,$res) {
   $props['taxon'] = $es->get($params)['_source'];
   $props['title'] = $props['taxon']['scientificName'];
 
-  $stats = ['eoo','eoo_historic','eoo_recent','aoo','aoo_historic','aoo_recent','clusters','clusters_historic','clusters_recent','risk_assessment'];
+  $stats = ['eoo','eoo_historic','eoo_recent','aoo','aoo_historic','aoo_recent','clusters','clusters_historic','clusters_recent','risk_assessment','count'];
   $props['stats']=$stats;
 
   foreach($stats as $s) {
